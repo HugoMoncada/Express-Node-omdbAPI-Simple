79ee749e84d1369002df7b86ab421198a922c90d
Hugo Moncada <hugo_moncada@outlook.com>
2020-07-25 17:39:36 -0500

    changes port

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.get("*", (req,res)=>{
 
 //====================
 
-app.listen(3000, ()=> console.log("App up!! port 3000"))
+app.listen(process.env.PORT || 3000, ()=> console.log("App up!! port 3000"))
 
 
 // by Hugo Moncada
